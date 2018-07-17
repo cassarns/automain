05398fff1e69957bc4e73688f3ded47ba53dd50b
Nick <nicholas.cassar@hotmail.com>
2018-07-17 13:56:47 -0600

    Updated pom and gitignore

--- src/test/java/com/nickcassar/automain/AppTest.java
+++ src/test/java/com/nickcassar/automain/AppTest.java
@@ -4,12 +4,14 @@ import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
 
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
+public class AppTest extends TestCase {
     /**
      * Create the test case
      *
@@ -33,6 +35,14 @@ public class AppTest
      */
     public void testApp()
     {
-        assertTrue( true );
-    }
+		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
+		Session session = sessionFactory.openSession();
+		session.beginTransaction();
+ 
+		AppUser user = new AppUser("firstuser");
+		session.save(user);
+ 
+		session.getTransaction().commit();
+		session.close();
+	}
 }
