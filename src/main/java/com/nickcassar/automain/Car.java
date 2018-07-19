d18592f5a6c453f9278f9f26295f22ab6df32ff1
nickcassar <nscassar@ucalgary.ca>
2018-07-19 14:37:24 -0600

    Started implementing Car

---
+++ src/main/java/com/nickcassar/automain/Car.java
@@ -0,0 +1,21 @@
+/**
+ * This is the main superclass for a Car that contains all information shared by all cars
+ */
+package com.nickcassar.automain;
+
+import java.io.Serializable;
+
+
+public class Car implements Serializable {
+
+  /**
+   * Class variables
+   */
+  
+
+  // Empty constructor for Hibernate
+  public Car() {
+  }
+
+
+}
